36c90309b4c5ecfccffbe15af6d503d2955bad56
Jens Tschirpig <jens1101@users.noreply.github.com>
2021-01-15 21:57:02 +0200

    Changed user that runs service file

--- first-boot.sh
+++ first-boot.sh
@@ -4,20 +4,20 @@ if [[ -f "./.env" ]]; then
     source "./.env"
 fi
 
-apt-get -qq update > /dev/null
+sudo apt-get -qq update > /dev/null
 echo "Apt Updated"
 
 curl -sSL https://get.docker.com | sh
 echo "Docker installed"
 
-apt-get -qq install -y libffi-dev libssl-dev python3 python3-pip > /dev/null
+sudo apt-get -qq install -y libffi-dev libssl-dev python3 python3-pip > /dev/null
 echo "Dependencies installed"
 
-pip3 -qq install docker-compose > /dev/null
+sudo pip3 -qq install docker-compose > /dev/null
 echo "Docker compose installed"
 
 docker-compose up -d --quiet-pull
 echo "Containers started"
 
-systemctl disable first-boot
+sudo systemctl disable first-boot
 echo "First boot service disabled"
