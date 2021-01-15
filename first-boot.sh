4ee12951524bde82b1ca37b55973990e3af08cfd
Jens Tschirpig <jens1101@users.noreply.github.com>
2021-01-15 19:45:44 +0200

    Created a first boot service
    Moved any setup that required internet access to a seperate service.

---
+++ first-boot.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+if [[ -f "./.env" ]]; then
+    source "./.env"
+fi
+
+apt-get -qq update > /dev/null
+echo "Apt Updated"
+
+curl -sSL https://get.docker.com | sh
+echo "Docker installed"
+
+apt-get -qq install -y libffi-dev libssl-dev python3 python3-pip > /dev/null
+echo "Dependencies installed"
+
+pip3 -qq install docker-compose > /dev/null
+echo "Docker compose installed"
+
+docker-compose up -d --quiet-pull
+echo "Containers started"
+
+systemctl disable first-boot
+rm -f /etc/systemd/system/first-boot.service ./.env
+
+reboot
