4c15b168b7ad60d81bc02ebb06ccd03b3067fc5a
Jens Tschirpig <jens1101@users.noreply.github.com>
2021-01-15 20:41:00 +0200

    Removed unnecessary "sudo"

--- first-boot.sh
+++ first-boot.sh
@@ -10,7 +10,7 @@ echo "Apt Updated"
 curl -sSL https://get.docker.com | sh
 echo "Docker installed"
 
-sudo usermod -aG docker pi
+usermod -aG docker pi
 echo "User access granted"
 
 apt-get -qq install -y libffi-dev libssl-dev python3 python3-pip > /dev/null
